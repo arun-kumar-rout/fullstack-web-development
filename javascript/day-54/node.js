cf88f72519d0dd7aff4d4421004a953d5a6f86ed
arun-kumar-rout <routarunkumar31@gmail.com>
2023-11-19 12:30:57 +0530

    added day 54

---
+++ javascript/day-54/node.js
@@ -0,0 +1,6 @@
+console.log("Hello World Dear 01");
+console.log("Hello World Dear 02");
+console.log("Hello World Dear 03");
+console.log("Hello World Dear 04");
+console.log("Hello World Dear 05");
+console.log("Hello World Dear 06");
--- javascript/day-54/script.js
+++ javascript/day-54/script.js
@@ -1,3 +1,4 @@
 // javascript is a client side interacting script
 alert("Hello World!");
-console.log("Code is running...")
+console.log("Code is running...");
+console.log(document);
