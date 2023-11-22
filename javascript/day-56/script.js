b1fd536eee3773ef443b171b2149946a5ab924eb
arun-kumar-rout <routarunkumar31@gmail.com>
2023-11-22 14:09:48 +0530

    added day 56

---
+++ javascript/day-56/script.js
@@ -0,0 +1,20 @@
+console.log("Hello World! hey");
+
+let age = 45;
+(age < 18) ? (console.log("You can drive")) : (console.log("You cannot drive"));
+
+// operators: 
+
+// ternary opperator
+a = 6;
+b = 8;
+let c = (a > b) ? (a-b) : (b-a);
+console.log(c)
+/*
+translet to:
+if (a > b){
+    let c = a - b;
+}else{
+    let c = b - a;
+}
+*/
